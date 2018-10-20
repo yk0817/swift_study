e15f7f6bf5c1581f955dc858269e91dca6258a1d
山本　憲 <em072029@gmail.com>
2018-10-20 16:52:27 +0900

    Initial Commit

---
+++ presentExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  presentExample
+//
+//  Created by 山本　憲 on 2018/10/20.
+//  Copyright © 2018年 山本　憲. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
